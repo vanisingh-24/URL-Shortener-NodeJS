89c81f11a9665daeb019f7e38ddba87ccf1626e6
vanisingh-24 <61752418+vanisingh-24@users.noreply.github.com>
2021-02-08 13:58:55 +0530

    shorturl route defined

--- app.js
+++ app.js
@@ -1,23 +1,40 @@
 const express = require('express');
-const connectDB = require('./config/db');
+const mongoose = require('mongoose');
+const Url = require('./models/Url');
+
+mongoose.connect('mongodb://localhost/urlShortener', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+//const connectDB = require('./config/db');
 
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: false}));
 
-app.get('/', (req,res) => {
-   res.render('index')
+app.get('/', async (req,res) => {
+   const shortUrl = await ShortUrl.find();
+   res.render('index', {shortUrl: shortUrl});
+});
+
+app.post('/Url', async (req,res) => {
+   await Url.create({full: req.body.fullUrl});
+   res.redirect('/');
 });
 
 //Connect to databse
-connectDB();
-
-app.use(express.json({extended: false}));
+//connectDB();
 
 //Define routes
-app.use('/', require('./routes/index'));
-app.use('/api/url', require('./routes/url'));
+//app.use('/', require('./routes/index'));
+//app.use('/api/url', require('./routes/url'));
 
 // const PORT = 5000;
+app.get('/:shortUrl',async (req,res) => {
+   const shortUrl = await ShortUrl.findOne({short: req.param.shortUrl });
+   if(shortUrl == null) return res.sendStatus(404);
+});
 
 app.listen(process.env.PORT || 5000);
--- models/Url.js
+++ models/Url.js
@@ -1,10 +1,21 @@
 const mongoose = require('mongoose');
+const shortId = require('shortid');
 
 const urlSchema = new mongoose.Schema({
-    urlCode: String,
-    longUrl: String,
-    shortUrl: String,
-    date: {type: String, default: Date.now}
+    full: {
+        type: String,
+        required: true
+    },
+    short: {
+        type: String,
+        required: true,
+        default: shortId.generate
+    },
+    clicks: {
+        type: Number,
+        required: true,
+        default: 0
+    }
 });
 
 module.exports = mongoose.model('Url', urlSchema);
